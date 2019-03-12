2381af43110f695e953c70b605fa82ca934a2a16
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-03-12 08:09:13 +0100

    scroll to top plugin was improved

--- src/assets/js/scroll-to-top.js
+++ src/assets/js/scroll-to-top.js
@@ -4,11 +4,13 @@ PortfolioTheme.ScrollToTop = (function () {
 
     const init = () => {
         const btn = document.querySelector('#scrollToTop');
-        const _partialThrottle = throttle(_handleScroll);
+        btn && _showOrHide(btn);
+        const _partialThrottle = throttle(_showOrHide);
         btn && addEventListener('scroll', () => _partialThrottle(btn));
+        btn && btn.addEventListener('click', () => _scrollToTop());
     };
 
-    const _handleScroll = btn => {
+    const _showOrHide = btn => {
         if (scrollY >= 200) {
             btn.style.visibility = 'visible';
             btn.style.opacity = '1';
@@ -16,6 +18,13 @@ PortfolioTheme.ScrollToTop = (function () {
             btn.style.visibility = 'hidden';
             btn.style.opacity = '0';
         }
+    };
+
+    const _scrollToTop = () => {
+        scrollTo({
+            behavior: "smooth",
+            top: 0
+        });
     };
 
     return {
