7e08f27dad3849e02a013287290484a4a28e8c8f
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-04-01 14:54:30 +0200

    single-post was renamed into single-post-wgt

--- general/sidebars.php
+++ general/sidebars.php
@@ -7,7 +7,7 @@ function theme_register_post_sidebar_left()
         'id'            => 'theme-post-sidebar-left',
         'description'   => 'Left sidebar at single post page',
         'class'         => '',
-        'before_widget' => '<div id="%1$s" class="widget single-post u-shadow-3 %2$s">',
+        'before_widget' => '<div id="%1$s" class="widget single-post-wgt u-shadow-3 %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<div class="theme-heading theme-heading--with-underline theme-heading--with-underline-centered project-content__heading "><h3 class="theme-heading__text">',
         'after_title'   => '</h3></div>'
