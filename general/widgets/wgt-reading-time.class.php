1930862e2b22c632c10bd1d891a7f5aafc7bca98
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-04-01 13:28:30 +0200

    added arguments support trait and widget method was implemented

--- general/widgets/wgt-reading-time.class.php
+++ general/widgets/wgt-reading-time.class.php
@@ -1,10 +1,23 @@
 <?php
 
+
+require_once 'traits/arguments-support.trait.php';
+
 class Wgt_ReadingTime extends WP_Widget
 {
+    use ArgumentsSupport;
+
     public function __construct()
     {
+        $args = [
+            'description' => 'Display time of reading'
+        ];
+        parent::__construct('wgt-reading-time', 'Reading time', $args);
+    }
 
+    public function widget($args, $instance)
+    {
+        require_once 'wgt-reading-time.view.php';
     }
 }
 
