516c3b4304a1aef4ea1e2c732bcb4d6b89995873
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-04-01 14:35:44 +0200

    current title value was passed to the input field

--- general/widgets/wgt-reading-time.form.php
+++ general/widgets/wgt-reading-time.form.php
@@ -1,2 +1,2 @@
 <label for="<?= $this->get_field_id('title') ?>">Title: </label>
-<input type="text" id="<?= $this->get_field_id('title') ?>" name="<?= $this->get_field_name('title') ?>">
+<input type="text" id="<?= $this->get_field_id('title') ?>" name="<?= $this->get_field_name('title') ?>" value="<?= $this->getWgtTitle($instance) ?>">
