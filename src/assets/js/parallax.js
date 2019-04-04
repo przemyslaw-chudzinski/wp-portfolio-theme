b2ad98ec9dd676f63cabf0715a075752f4161e95
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-04-04 13:43:16 +0200

    parallax fixed

--- src/assets/js/parallax.js
+++ src/assets/js/parallax.js
@@ -1,6 +1,7 @@
 PortfolioTheme.Parallax = (function () {
 
     const init = () => {
+        if (window.innerWidth < 991) return;
         const header = document.querySelector('header[data-has-parallax]');
         header && (header.style.backgroundAttachment = 'fixed');
         header && addEventListener('scroll', event => _scrollHandler(header, event));
@@ -8,7 +9,7 @@ PortfolioTheme.Parallax = (function () {
 
     const _scrollHandler = header => {
         const userSpeed = header.dataset.parallaxSpeed ? parseFloat(header.dataset.parallaxSpeed) : null;
-        const speed = userSpeed || 0.5;
+        const speed = userSpeed || 0.3;
         header.style.backgroundPositionY = -1 * scrollY * speed + 'px';
     };
 
