8dd6ed82cde890ffda4ac922fbe721fac5eaa677
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-04-04 12:19:06 +0200

    new control was added and style was improved

--- general/widgets/wgt-reading-time/wgt-reading-time.form.php
+++ general/widgets/wgt-reading-time/wgt-reading-time.form.php
@@ -1,2 +1,9 @@
-<label for="<?= $this->get_field_id('title') ?>">Title: </label>
-<input type="text" id="<?= $this->get_field_id('title') ?>" name="<?= $this->get_field_name('title') ?>" value="<?= $this->getWgtTitle($instance) ?>">
+<p>
+    <label for="<?= $this->get_field_id('title') ?>">Title: </label>
+    <input type="text" id="<?= $this->get_field_id('title') ?>" name="<?= $this->get_field_name('title') ?>" value="<?= $this->getWgtTitle($instance) ?>" class="widefat">
+</p>
+
+<p>
+    <label for="<?= $this->get_field_id('words') ?>">Words per min: </label>
+    <input type="text" id="<?= $this->get_field_id('words') ?>" name="<?= $this->get_field_name('words') ?>" value="<?= (int) $this->getWgtWordsPerMin($instance) ?>" class="widefat">
+</p>
